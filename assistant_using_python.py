732e0b92601fa746f1c5a8f015831fd49c810154
Induwara Uthsara <induwarauthsara@gmail.com>
2021-03-24 18:47:30 +0530

    Update assistant_using_python.py

--- assistant_using_python.py
+++ assistant_using_python.py
@@ -1,6 +1,6 @@
 import keyboard, time
 item_list = ["notepad", "chrome", "firefox", "calc", "mspaint", "explorer", "wordpad" ]
-print("1.notepad\n2.chrome\n3.firefox\n4. Calculator\n5.  Paint\n6. My Compueter (Explorer)\n7. WordPad\n  ")
+print("1.notepad\n2.chrome\n3.firefox\n4. Calculator\n5.  Paint\n6. My Compueter (Explorer)\n7. WordPad\n  \n asawa harida dan binath kommlo")
 choice = input("enter Your Choice :")
 keyboard.press("win+r")
 keyboard.release("win")
